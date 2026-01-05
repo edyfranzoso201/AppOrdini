532f35d031b065e9359c8a11b365a18b68a5e1ff
edyfranzoso201 <edy.franzoso@gmail.com>
2026-01-06 00:43:27 +0100

    Update redis.js

--- api/lib/redis.js
+++ api/lib/redis.js
@@ -1,6 +1,5 @@
 import { Redis } from '@upstash/redis';
 
-// Chiavi per organizzare i dati in Redis
 export const KEYS = {
   ORDERS: 'orderflow:orders',
   USERS: 'orderflow:users',
@@ -8,7 +7,6 @@ export const KEYS = {
   ACTIVITY_LOG: 'orderflow:activity_log'
 };
 
-// Crea connessione Redis usando le variabili d'ambiente di Vercel
 export function getRedis() {
   return new Redis({
     url: process.env.KV_REST_API_URL,
